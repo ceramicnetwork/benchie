c8ca1483a502c6cbda4eaa7a5a57c755e21e92ef
stephhuynh18 <stephhuynh18@users.noreply.github.com>
2022-03-29 07:52:13 -0700

    Add ability to run specific files based on pattern

--- src/benchie/runner.ts
+++ src/benchie/runner.ts
@@ -5,14 +5,62 @@ import {
   ScenarioResult,
 } from "./scenario-result.js";
 import { scenarios } from "./benchmark.js";
+import path from "path";
+import fs from "fs";
+
+const createFileGlob = (pattern: string, absolutePath: string): string => {
+  // if the pattern is not a path create a glob that returns files that include the pattern
+  if (path.basename(pattern) === pattern) {
+    const dir = path.dirname(absolutePath);
+    const base = path.basename(absolutePath);
+
+    return path.resolve(dir, "**", `*${base}*`);
+  }
+
+  // if the pattern is a path create a glob that returns files that extend the path
+  return `${absolutePath}*`;
+};
+
+const getFiles = async (pattern: string): Promise<string[]> => {
+  const globs = [];
+  const absolutePath = path.resolve(process.cwd(), pattern);
+
+  // if its a dir or a file
+  if (!glob.isDynamicPattern(pattern)) {
+    globs.push(createFileGlob(pattern, absolutePath));
+
+    // if the pattern is not an actual file or is a directory we will treat it as a directory
+    if (!fs.existsSync(pattern) || fs.statSync(pattern).isDirectory()) {
+      const dirGlob = path.resolve(absolutePath, "**", "*.bench.js");
+      globs.push(dirGlob);
+    }
+  } else {
+    globs.push(pattern);
+  }
+
+  const allFiles = await glob(globs, {
+    absolute: true,
+    onlyFiles: true,
+    unique: true,
+    ignore: [
+      path.resolve(absolutePath, "**", "node_modules", "**"),
+      path.resolve("**", "node_modules", "**"),
+    ],
+  });
+
+  return allFiles.filter((file) => file.endsWith(".bench.js"));
+};
 
 export class Runner {
-  constructor(readonly rootDir: URL) {}
+  constructor(readonly pattern: string) {}
 
   async run(): Promise<IScenarioResult[]> {
-    const benchFiles = await glob(`${this.rootDir.pathname}/**/*.bench.js`).then(
-      (results) => results.filter((path) => !path.includes("node_modules"))
-    );
+    const benchFiles = await getFiles(this.pattern);
+
+    if (benchFiles.length === 0) {
+      throw new Error(`No .bench.js files found using pattern ${this.pattern}`);
+    }
+
     let filenamesAndScenarios: IScenariosPerFile[] = [];
     for (let entry of benchFiles) {
       const fullFilePath = new URL(`file://${entry}`);
